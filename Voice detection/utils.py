b24bab95e543d6c07a6f54c3b0c7c6364b76d3c4
peyman iravani <peyman.iravani.altranitalia@windtre.it>
2020-04-20 21:26:40 +0200

    update

--- Voice detection/utils.py
+++ Voice detection/utils.py
@@ -23,3 +23,32 @@ def get_label(audio_config):
         if audio_config[feature]:
             label += f"{feature}-"
     return label.rsplit("-")
+
+
+def get_dropout_str(dropout, n_layers=3):
+    if isinstance(dropout, list):
+        return "_".join([str(d) for d in dropout])
+    elif isinstance(dropout, float):
+        return "_".join([str(dropout) for i in range(n_layers)])
+
+
+def get_first_letter(emotions):
+    return "".join(sorted([e[0].upper() for e in emotions]))
+
+
+def extract_feature(filename, **kwargs):
+    mfcc = kwargs.get("mfcc")
+    chroma = kwargs.get("chroma")
+    mel = kwargs.get("mel")
+    contrast = kwargs.get("contrast")
+    tonnetz = kwargs.get("tonnetz")
+    with soundfile.SoundFile(file_name) as sound_file:
+        X = sound_file.read(dtype="float32")
+        sample_rate = sound_file.samplerate
+        if chroma or contrast:
+            stft = np.abs(librosa.stft(X))
+        result = np.array([])
+        if mfcc:
+            mfccs = np.mean(librosa.feature.mfcc(
+                y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
+            result = np.hstack((result, mfccs))
